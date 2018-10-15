6d17ea2b2942a370b698b47715d82ccdfa308a37
yashp2807 <yash.panchal@innovaccer.com>
2018-10-15 18:44:48 +0530

    changes in datatypes

--- db/migrate/20181010063052_note.rb
+++ db/migrate/20181010063052_note.rb
@@ -1,6 +1,6 @@
 class Note < ActiveRecord::Migration[5.0]
   def up
-    execute "create table l2.note (empi varchar(255),local_member_id varchar(255),encounter_id varchar(255),provider_name varchar(255),practice_name varchar(255),note_type varchar(255),note varchar,author_name varchar(255),source_id varchar(255),source_name varchar(255),source_type varchar(255),workflow_id varchar(255),author varchar(255),vendor_version varchar(55),vendor_name varchar(255),aco_id varchar(55),aco_name varchar(255),source_file_name varchar(510),Workspace_id varchar(255),birth_date date,note_date date,author_date datetime,ingestion_datetime datetime,author_id integer,provider_npi bigint,practice_npi bigint) distkey(empi) sortkey(empi);"
+    execute "create table l2.note (empi varchar(255),local_member_id varchar(255),encounter_id varchar(255),provider_name varchar(255),practice_name varchar(255),note_type varchar(255),note varchar,author_name varchar(255),source_id varchar(255),source_name varchar(255),source_type varchar(255),workflow_id varchar(255),author varchar(255),vendor_version varchar(55),vendor_name varchar(255),aco_id varchar(55),aco_name varchar(255),source_file_name varchar(510),Workspace_id varchar(255),birth_date date,note_date date,author_date datetime,ingestion_datetime datetime,author_id integer,provider_npi string,practice_npi string) distkey(empi) sortkey(empi);"
   end
   def down
     execute "drop table l2.note";
