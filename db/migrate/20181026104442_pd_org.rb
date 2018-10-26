05b1a55020f8a3055e9fe6749e5602f93cfead68
Yash <yash.panchal@innovaccer.com>
2018-10-26 17:23:22 +0530

    Added activity,attribution and org tables

---
+++ db/migrate/20181026104442_pd_org.rb
@@ -0,0 +1,8 @@
+class PdOrg < ActiveRecord::Migration[5.0]
+  def up
+    execute "create table l2.pd_org (prvn varchar(200),prnpi varchar(20),spctcd varchar(30),pspn varchar(200),pradd varchar(200),prct varchar(100),prst varchar(50),przp varchar(20),prctr varchar(100),preml varchar(100),prcn varchar(200),pcpflg varchar(10),fcid varchar(100),fcnm varchar(200),fcnpi varchar(20),fcadd varchar(200),fcct varchar(100),fcst varchar(50),fczp varchar(20),fcctr varchar(100),pfflg varchar(10),psdt date,pedt date,orgtin varchar(100),orgid varchar(100),orgnm varchar(200),rgid varchar(100),rgnm varchar(200),acoid varchar(100),aconm varchar(200),pt varchar(100),prlf varchar(10)) distkey(prnpi) sortkey (prnpi);"
+  end
+  def down
+    execute "drop table l2.pd_org";
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024102534) do
+ActiveRecord::Schema.define(version: 20181026104442) do
 
   create_table "attribution", id: false, force: :cascade do |t|
     t.string   "empi",                         limit: 255
