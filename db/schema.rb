e441692c0ae34c0eeb7d21d8a642654d41f18923
Yash <yash.panchal@innovaccer.com>
2018-11-29 17:50:23 +0530

    addition of practice_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181115172536) do
+ActiveRecord::Schema.define(version: 20181129121722) do
 
 end
