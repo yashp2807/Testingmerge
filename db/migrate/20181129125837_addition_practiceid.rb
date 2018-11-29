92012bb4f8f0e6e47704d3ea0ab4486fd8b96c3e
Yash <yash.panchal@innovaccer.com>
2018-11-29 18:31:33 +0530

    addition of new column practice_id

---
+++ db/migrate/20181129125837_addition_practiceid.rb
@@ -0,0 +1,8 @@
+class AdditionPracticeid < ActiveRecord::Migration[5.0]
+  def up
+    execute "alter table l2.provider_org_hierarchy add column practice_id varchar(100);"
+  end
+  def down
+    execute "alter table l2.provider_org_hierarchy drop column practice_id;"
+  end
+end
