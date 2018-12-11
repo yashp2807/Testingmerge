3b1a09dc90dfda29b7d3673903401e550e8b4c3e
Yash <yash.panchal@innovaccer.com>
2018-12-11 17:16:18 +0530

    addition of npi and ontology

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181129125837) do
+ActiveRecord::Schema.define(version: 20181211113456) do
 
 end
